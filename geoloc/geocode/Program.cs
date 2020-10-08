f04df6e849f6388b2f935bccbb734f6c7700b508
LAPTOP-VDLHET1H\lajoh <la.johan.lindstrom@gmail.com>
2020-10-08 09:40:22 +0200

    Jobat med Övningar

--- geoloc/geocode/Program.cs
+++ geoloc/geocode/Program.cs
@@ -1,25 +1,73 @@
 ﻿using NGeoNames;
 using NGeoNames.Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace geocode
 {
     class Program
     {
+        const string _resoucePatch = ".\\Resources\\SE.txt";
+        
         static readonly IEnumerable<ExtendedGeoName> _locationNames;
         static readonly ReverseGeoCode<ExtendedGeoName> _reverseGeoCodingService;
 
+        static readonly (double lat, double lng)_gavlePosistion;
+        static readonly (double lat, double lng) _uppsalaPosistion;
         static Program()
         {
             _locationNames = GeoFileReader.ReadExtendedGeoNames(".\\Resources\\SE.txt");
             _reverseGeoCodingService = new ReverseGeoCode<ExtendedGeoName>(_locationNames);
+
+            _gavlePosistion = (60.674622, 17.141830);
+            _uppsalaPosistion = (59.858562, 17.638927);
         }
 
         static void Main(string[] args)
         {
+
             // 1. Hitta de 10 närmsta platserna till Gävle (60.674622, 17.141830), sorterat på namn.
+            ListaGavlePosition();
+
             // 2. Hitta alla platser inom 200 km radie till Uppsala (59.858562, 17.638927), sorterat på avstånd.
+            Console.WriteLine("2. Uppsala");
+            
+            ListUppsalaPosition();
             // 3. Lista x platser baserat på användarinmatning.
+        
+        }
+        static void ListUppsalaPosition()
+
+        {
+
+            var radius = 200 * 1000;
+            var nearestUppsalaPosition = _reverseGeoCodingService.RadialSearch(_uppsalaPosistion.lat, _uppsalaPosistion.lng, radius, 50);
+
+            nearestUppsalaPosition = nearestUppsalaPosition.OrderBy(x => x.DistanceTo(_uppsalaPosistion.lat, _uppsalaPosistion.lng));
+
+            foreach (var potition in nearestUppsala)
+            {
+                var uppsalaDistance = potition.DistanceTo(lat, lng,);
+            
+            
+
+            }
+        }
+
+
+
+        static void ListaGavlePosition()
+
+        {
+            var nearestGavle = _reverseGeoCodingService.RadialSearch(_gavlePosistion.lat, _gavlePosistion.lng, 10);
+
+            nearestGavle = nearestGavle.OrderBy(p => p.Name);
+
+            foreach (var position in nearestGavle)
+            {
+                Console.WriteLine($"{position.Name}, lat: {position.Latitude}, lng: {position.Longitude}");
+            }
         }
     }
 }
--- leapyear/leapyearcalculator/Program.cs
+++ leapyear/leapyearcalculator/Program.cs
@@ -6,9 +6,54 @@ namespace leapyearcalculator
     {
         static void Main(string[] args)
         {
-            // Räkna ut hur många skottår som passerat mellan två inmatade värden.
+            int firstYear = int.Parse(args[0]);
+            int secondYear = int.Parse(args[1]);
 
-            // DateTime.IsLeapYear(year) är en metod man kan använda.
+            int maxYear = Math.Max(firstYear,secondYear);
+            int minYear = Math.Min(firstYear, secondYear);
+
+            int leapYearCount = 0;
+
+            for (int year = minYear; year < maxYear; year++)
+            {
+                bool isLeapYear = DateTime.IsLeapYear(year);
+
+                if (isLeapYear)
+                {
+                    leapYearCount++;
+                }
+            }
+            Console.WriteLine($"Encountered {leapYearCount} leap years from {minYear} to {maxYear}");
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            //räkna ut hår många skottår det är mellan två värden.
+
+           //for (int i = 1984; i < 2020; i++)
+            //{
+                //DateTime.IsLeapYear(i).ToString();
+
+                //if (true)
+                //{
+                    //Console.WriteLine(i + "is a leapyer");
+                //}
+                
+            //}
+
+            
+
+
+
         }
     }
 }
